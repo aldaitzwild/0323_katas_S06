57209c5acc25ff32c3e4ddc9f605b0b1386945b9
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-04-25 08:39:39 +0200

    Add the kata for tuesday

---
+++ tuesday.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 1/ Créer une fonction getRange() qui prend un chiffre positif en entrée 
+ * (si un négatif est passé la fonction renvoit un tableau vide) et qui renvoit 
+ * un tableau contenant toutes les valeurs de 0 au chiffre passé. 
+ * Avant de le renvoyer la fonction devra également ranger aléatoirement les éléments 
+ * du tableau.
+ * 
+ * 2/ Afficher les éléments retournés par la fonction sous la forme : 
+ * => X - X - X - X - X ...
+ */

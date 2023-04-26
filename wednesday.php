2f3dc9289b63ce006896bab9b1937a7eb04b1d26
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-04-26 09:29:36 +0200

    Add the correction for wednesday

--- wednesday.php
+++ wednesday.php
@@ -6,6 +6,18 @@
 
  function whoIsTheWinner(array $grid): string
  {
+    if($grid[0][0] == $grid[0][1] && $grid[0][0] == $grid[0][2]) return $grid[0][0];
+    if($grid[1][0] == $grid[1][1] && $grid[1][0] == $grid[1][2]) return $grid[1][0];
+    if($grid[2][0] == $grid[2][1] && $grid[2][0] == $grid[2][2]) return $grid[2][0];
+
+    if($grid[0][0] == $grid[1][0] && $grid[0][0] == $grid[2][0]) return $grid[0][0];
+    if($grid[0][1] == $grid[1][1] && $grid[0][1] == $grid[2][1]) return $grid[0][1];
+    if($grid[0][2] == $grid[1][2] && $grid[0][2] == $grid[2][2]) return $grid[0][2];
+
+
+    if($grid[0][0] == $grid[1][1] && $grid[0][0] == $grid[2][2]) return $grid[0][0];
+    if($grid[0][2] == $grid[1][1] && $grid[0][2] == $grid[2][0]) return $grid[0][2];
+
     return 'personne :-(';
  }
 
